5bf19d87c525509e5ed28fe80609f28e8ab68b61
tengfei31 <776095293@qq.com>
2020-09-02 18:23:13 +0800

    测试上传到github

---
+++ server.go
@@ -0,0 +1,40 @@
+/*
+ * @Author: wtf
+ * @Date: 2020-09-02 14:45:10
+ * @LastEditors: wtf
+ * @LastEditTime: 2020-09-02 17:08:56
+ * @Description: plase write Description
+ */
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+
+	pb "go-grpc/proto"
+)
+
+
+//rpc接收结构
+type SearchService struct{}
+
+func (s *SearchService) Search(c context.Context, r *pb.SearchRequest) (*pb.SearchResponse, error) {
+	log.Printf("recv from client: %s", r.GetRequest())
+	return &pb.SearchResponse{Response: r.GetRequest() + " Server"}, nil
+}
+
+//端口号
+const PORT = "9001"
+
+func main() {
+	server := grpc.NewServer()
+	pb.RegisterSearchServiceServer(server, &SearchService{})
+
+	lis, err := net.Listen("tcp", ":" + PORT)
+	if err != nil {
+		log.Fatalf("net.Listen err: %v", err)
+	}
+	server.Serve(lis)
+}
